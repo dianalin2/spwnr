a79189fbd4be4914fa91d42d85b1a64e2a2a16ee
Andrew Hong <honga010807@gmail.com>
2025-07-23 14:17:34 -0400

    slight changes to import and logging the start of the server

--- server/main.go
+++ server/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"net/http"
+	"github.com/go-chi/chi/v5"
 	"mc.honki.ng/spwnr/logging"
 	"mc.honki.ng/spwnr/api"
-	"github.com/go-chi/chi/v5"
 )
 
 func createRoutes(logger logging.Logger) *chi.Mux {
@@ -17,22 +17,22 @@ func createRoutes(logger logging.Logger) *chi.Mux {
 
 func main() {
 	logger, err := logging.CreateFileLogger("server.log")
+	addr := ":8080"
 	if err != nil {
 		panic("Failed to create logger: " + err.Error())
 	}
 
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    addr,
 		Handler: createRoutes(logger),
 	}
 
 	// Start the server
+	logger.Log("Starting server on port " + addr)
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		logger.Log("Server failed to start: " + err.Error())
 		return
 	}
-
-	logger.Log("Server started successfully")
 
 	defer func() {
 		if err := server.Close(); err != nil {
